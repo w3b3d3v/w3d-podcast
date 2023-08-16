15ba1fee55665660b196c00ae71358edcee7de51
Daniel Cukier <danicuki@gmail.com>
2023-08-16 17:40:33 -0300

    more functional version

--- index.js
+++ index.js
@@ -24,26 +24,21 @@ async function textToAudio(text, languageCode, gender, voiceName) {
 }
 
 async function convertConversation(fullConversation) {
-  let audioData = Buffer.alloc(0)
-  const tasks = []
-
-  for (const [role, message] of fullConversation) {
-    if (role === "user") {
-      tasks.push(textToAudio(message, "pt-BR", "MALE", "pt-BR-Wavenet-B"))
-    } else {
-      tasks.push(textToAudio(message, "pt-BR", "FEMALE", "pt-BR-Wavenet-A"))
-    }
-  }
-
-  const audios = await Promise.all(tasks)
-
-  audios.forEach(audio => {
-    audioData = Buffer.concat([audioData, audio])
-  })
-
+  const audios = await Promise.all(
+    fullConversation.reduce((acc, [role, message]) => {
+      if (role === "user") {
+        acc.push(textToAudio(message, "pt-BR", "MALE", "pt-BR-Wavenet-B"))
+      } else {
+        acc.push(textToAudio(message, "pt-BR", "FEMALE", "pt-BR-Wavenet-A"))
+      }
+      return acc
+    }, [])
+  )
   // Salve o áudio em um arquivo
-  const writeFile = util.promisify(fs.writeFile)
-  await writeFile("tts_conversation.mp3", audioData)
+  await util.promisify(fs.writeFile)(
+    "tts_conversation.mp3",
+    audios.reduce((acc, audio) => Buffer.concat([acc, audio]), Buffer.alloc(0))
+  )
   console.log("Conversão concluída! O arquivo 'tts_conversation.mp3' foi salvo no diretório atual.")
 }
 
